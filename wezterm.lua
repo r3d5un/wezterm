76077f1d04cde25817e5d752c399aaa9765278c0
r3d5un <r3d5un@pm.me>
2023-10-14 16:38:52 +0200

    Update color scheme

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "tokyonight_night"
 config.font_size = 11.0
 config.enable_tab_bar = false
 config.animation_fps = 60
